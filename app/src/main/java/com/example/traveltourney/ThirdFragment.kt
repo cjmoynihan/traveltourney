7e4fa0018608258d8d467843095145a512b93865
martin-dickie <mbdickie@umass.edu>
2020-09-07 17:15:20 -0400

    Made third button

--- app/src/main/java/com/example/traveltourney/ThirdFragment.kt
+++ app/src/main/java/com/example/traveltourney/ThirdFragment.kt
@@ -9,26 +9,26 @@ import android.widget.Button
 import androidx.navigation.fragment.findNavController
 
 /**
- * A simple [Fragment] subclass as the second destination in the navigation.
+ * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class SecondFragment : Fragment() {
+class ThirdFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+        return inflater.inflate(R.layout.fragment_third, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_second).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        view.findViewById<Button>(R.id.frag_3_button_1).setOnClickListener {
+            findNavController().navigate(R.id.action_thirdFragment_to_FirstFragment)
         }
-        view.findViewById<Button>(R.id.button_second_to_third).setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_thirdFragment)
+        view.findViewById<Button>(R.id.frag_3_button_2).setOnClickListener {
+            findNavController().navigate(R.id.action_thirdFragment_to_SecondFragment)
         }
     }
 }
